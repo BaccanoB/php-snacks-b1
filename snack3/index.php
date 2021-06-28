344599f913e56dbe2b472440bba25299cc738c7a
BaccanoB <beatrice.baccano@yahoo.it>
2021-06-28 16:08:28 +0200

    terminato snack3

--- snack3/index.php
+++ snack3/index.php
@@ -1,9 +1,16 @@
 <?php 
+    $i;
+    while($i < 15){
+        for ($i=0; $i<15; $i++) { 
+            $number = rand(1,50);
 
-    $numbers=[];
-    for ($i=0; $i<15; $i++) { 
-        $numbers += rand(1,50);
-    }
+            if(!in_array($number,$numbers)){
+                $numbers[]=$number;
+            }
+        }
+
+        $i++;
+    };
 
     print_r($numbers);
-?>
+    ?>
